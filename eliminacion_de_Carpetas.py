f3793d9c48a403a6aed7e3502cd21a22f0bb26df
adrià Rodriguez Sanchez <151919370+adriarodrisan@users.noreply.github.com>
2024-04-04 10:16:42 +0200

    inicio con problemas

---
+++ eliminacion_de_Carpetas.py
@@ -0,0 +1,12 @@
+import os
+class borrar_carpetas():
+    def __init__(self):
+        des
+    def borrar_carpeta_vacia():
+        borrar=borrar_contenedor()
+    def borrar_contenedor():
+        del contenedor
+    def borrar_carpeta():
+        borrar=borrar_contenido()
+    def borrar_contenido():
+        del contenido
